4033b9f17df6e8cb6943c87abd3c90ec57b113a8
0xAli <theekmekci0@gmail.com>
2023-09-11 17:37:30 +0800

    Update Payment.tsx

--- core-protocol/Components/Payment.tsx
+++ core-protocol/Components/Payment.tsx
@@ -9,66 +9,69 @@ const Payment = () => {
   return (
     <div className="max-w-screen-sm flex flex-wrap items-center justify-between mx-auto p-4 mt-16">
 
+        <div className='bg-white rounded-3xl w-full p-8'>
 
-              <div className='bg-white rounded-xl w-full p-8'>
-
-            <div className='flex mb-8 border rounded-lg bg-gray-200 overflow-hidden'>
+            <div className='flex mb-8 border rounded-lg bg-gray-300 overflow-hidden'>
                 <button 
                     onClick={() => setActiveTab('wallet')}
-                    className={`text-black w-1/2 border-gray-800 h-8 ${activeTab === 'wallet' ? 'bg-white' : ''}`}>
+                    className={`text-black text-sm w-1/2 border-gray-800 h-9 ${activeTab === 'wallet' ? 'bg-white' : ''}`}>
                     Pay With Wallet
                 </button>
                 <button 
                     onClick={() => setActiveTab('qr')}
-                    className={`text-black w-1/2 h-8 ${activeTab === 'qr' ? 'bg-white' : ''}`}>
+                    className={`text-black text-sm w-1/2 h-9 ${activeTab === 'qr' ? 'bg-white' : ''}`}>
                     Pay With QR Code
                 </button>
             </div>
 
-
-
-
-                <div className='text-xl'>
+            <div className='flex justify-between'>
+                <div className='text-l sm:text-2xl'>
                     Pay the testshop 
                 </div>
-                <div className='text-3xl mt-6'>
-                    $1,456.67
+                <div>
+                    Time remaining
+                    <p className='text-red-600 '>09:59</p>
                 </div>
 
-                <div className='text-2xl mt-12 flex justify-between'>
+            </div>
 
-                    <div>
-                        264.29 APT
-                    </div>
+            <div className='text-xl sm:text-2xl'>
+                $1,456.67
+            </div>
 
-                    <div>
+            <div className='text-xl sm:text-2xl mt-10 flex justify-between'>
+                <div>
+                    264.29 APT
+                </div>
+
+                    {/*<div>
                     Pay with
-                    {/* dropdown for currency select */}
-                    </div>
-                    
-                </div>
-                <hr className="h-px my-8 bg-gray-800 border-0 dark:bg-gray-700" />
+                     dropdown for currency select 
+                    </div>*/}
+            </div>
+            <hr className="h-px my-8 bg-gray-800 border-0 dark:bg-gray-700" />
 
-                <div className='p-2'>
-                    <div className='flex justify-between'>
-                        <p className='text-xl'> Cart </p>
-                        <p className='text-xl'> $1,456.67 </p>
-                    </div>
-
-                    <div className='flex justify-between'>
-                        <p className='text-xl'> Transaction Fee </p>
-                        <p className='text-xl'> $0.025 </p>
-                    </div>
+            <div className='p-2'>
+                <div className='flex justify-between'>
+                    <p className='text-xl'> Cart </p>
+                    <p className='text-xl'> $1,456.67 </p>
                 </div>
 
-                <div className='center flex w-full justify-center mt-24'>
-                   <button className='border text-black w-full border-gray-800	 rounded-xl h-12'> Connect Wallet </button>
+                <div className='flex justify-between'>
+                    <p className='text-xl'> Transaction Fee </p>
+                    <p className='text-xl'> $0.025 </p>
                 </div>
-                <div className='center flex w-full justify-center mt-2'>
-                   <button className='text-white w-full bg-black rounded-xl h-12'> Pay now </button>
-                </div>
+            </div>
 
-              </div>
+            <div className='center flex w-full justify-center mt-24'>
+                <button className='border text-black w-full border-gray-800	 rounded-xl h-12'> Connect Wallet </button>
+            </div>
+            <div className='center flex w-full justify-center mt-2'>
+                <button className='text-white w-full bg-black rounded-xl h-12'> Pay now </button>
+            </div>
+
+            
+        </div>
 
     </div>
   )
