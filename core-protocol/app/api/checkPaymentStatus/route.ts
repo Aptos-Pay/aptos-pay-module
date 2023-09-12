23445e74c9bb07cde5326a894eb09975fb3b4768
DanielHighETH <danielhigheth@gmail.com>
2023-09-13 01:48:01 +0800

    Changef amount to orderId

--- core-protocol/app/api/checkPaymentStatus/route.ts
+++ core-protocol/app/api/checkPaymentStatus/route.ts
@@ -7,10 +7,10 @@ export async function GET(request: Request) {
   const params = new URLSearchParams(
     request.url.slice(request.url.indexOf('?'))
   );
-  const amount = params.get('amount');
+  const orderId = params.get('orderId');
 
   const paymentStatus = await checkPaymentStatus(
-    amount as string,
+    orderId as string,
     process.env.ADDRESS as string,
     process.env.MODULE_ADDRESS as string
   );
@@ -18,7 +18,7 @@ export async function GET(request: Request) {
   return NextResponse.json(
     {
       success: paymentStatus.success,
-      status: paymentStatus.status,
+      status: paymentStatus.status  ,
     },
     {
       status: 200,
